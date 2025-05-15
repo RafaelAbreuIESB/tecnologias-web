cb500817bbfd1bb6b7b97a4325d2a2354ca3ac20
unknown <2312130113@ACADEMICO.IESB.BR>
2025-05-15 09:22:47 -0300

    add

---
+++ praticas/pratica04/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import InputEmail from "../components/InputEmail";
+
+function Login(){
+    return(
+        <form>
+            <InputEmail />
+            <button type="submit" onClick={() => {}}>
+                Entrar
+            </button>
+        </form>
+    );
+}
+export default Login;
