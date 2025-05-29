ff8d118b95c720cef6d228d2833abe1832a4fce7
unknown <2312130113@ACADEMICO.IESB.BR>
2025-05-29 10:12:46 -0300

    add

--- praticas/praticas04/front/src/App.jsx
+++ praticas/praticas04/front/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useContext } from "react";
+import { RotaContext } from "./contexts/RotaContext.jsx";
+import Cabecalho from "./components/Cabecalho.jsx";
+import Conteudo from "./components/Conteudo.jsx";
+import Listar from "./pages/Listar.jsx";
+import Editar from "./pages/Editar.jsx";
+import Novo from "./pages/novo.jsx";
 
 function App() {
+  const { setRota } = useContext(RotaContext);
   return (
-  <></>
+    <>
+      <Cabecalho />
+      <Conteudo>
+        {rota === "/listar" && <Listar />}
+        {rota === "/novo" && <Novo />}
+        {rota.substr(0, 7) === "/editar" && <Editar />}
+      </Conteudo>
+    </>
   );
-  }
-  export default App;
+}
+export default App;
---
+++ praticas/praticas04/front/src/components/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Menu";
+
+function Cabecalho() {
+    return (
+    <header>
+    <h1>@Contatos</h1>
+    <Menu />
+    </header>
+    );
+    }
+
+export default Cabecalho;
